698bce989166d797927d79225afb6d28e8c73fc5
Armindo Maurits <0882940@hr.nl>
2015-06-18 17:30:49 +0200

    Main aangepast

--- src/nl/hro/infanl018/opdracht5/Main.java
+++ src/nl/hro/infanl018/opdracht5/Main.java
@@ -1,5 +1,6 @@
 package nl.hro.infanl018.opdracht5;
 
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -9,7 +10,11 @@ import org.hibernate.cfg.Configuration;
 import org.hibernate.exception.JDBCConnectionException;
 
 public class Main {
+	private User armindo;
+	private User wim;
 	private SessionFactory sessionFactory;
+	private Category categoryAuto;
+	private Category categoryWitteAuto;
 
 	public static void main(String[] args) {
 		new Main();
@@ -22,24 +27,53 @@ public class Main {
 			System.out.println("Failed to connect.");
 			System.exit(1);
 		}
-		doSomething();
+		addUser();
 		sessionFactory.close();
 	}
 
 	private void doSomething() {
+	}
+
+	public void addUser() {
 		Session session = sessionFactory.openSession();
 		session.beginTransaction();
-		Set<PaymentDetails> details = new HashSet<PaymentDetails>();
-		details.add(new CreditCard("voornaam", "", 12, 2015));
-		details.add(new IDeal("voornaam", "123", "bank"));
-		User test = new User("voornaam", "achternaam", "email", "", details);
-		session.save(test);
-		session.getTransaction().commit();
-		session.beginTransaction();
-		User test2 = (User)session.get(User.class, 1);
+		Set<PaymentDetails> detailsArmindo = new HashSet<PaymentDetails>();
+		detailsArmindo.add(new CreditCard("Armindo", "7654321", 12, 2015));
+		detailsArmindo.add(new IDeal("Armindo", "1234567", "ING Bank"));
+		armindo = new User("Armindo", "Maurits", "0882940@hr.nl", "testpassword", detailsArmindo);
+
+		Set<PaymentDetails> detailsWim = new HashSet<PaymentDetails>();
+		detailsWim.add(new CreditCard("Wim", "6452100", 12, 2014));
+		detailsWim.add(new IDeal("Wim", "0012546", "ING Bank"));
+		wim = new User("Wim", "Procee", "iets@hr.nl", "testpassword", detailsArmindo);
+
+		session.save(armindo);
+		session.save(wim);
 		session.getTransaction().commit();
 		session.close();
-		System.out.println(test == test2);
-		System.out.println(test.getPaymentDetails() == test2.getPaymentDetails());
+	}
+
+	public void addAdvert() {
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		//vul de laatste dingen in, categorie etc.
+		Advert advert = new Advert("Witte auto te koop", "Hele mooie witte auto te koop. koop dit nu", 1500, true, new Date(), null, );
+		session.save(advert);
+		session.getTransaction().commit();
+		session.close();
+	}
+
+	public void addCategory() {
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+
+		categoryAuto = new Category(null , "Auto's");
+		session.save(categoryAuto);
+		session.getTransaction().commit();
+
+		categoryWitteAuto = new Category(categoryAuto, "Witte auto's");
+		session.save(categoryWitteAuto);
+		session.getTransaction().commit();
+		session.close();
 	}
 }
